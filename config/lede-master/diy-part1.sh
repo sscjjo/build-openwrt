9b59bdfa71d3abfaf6d27dceb0e9c49448c08f13
ccc <101983207+sscjjo@users.noreply.github.com>
2024-04-05 21:21:14 +0800

    diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -10,5 +10,5 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # other
-# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
